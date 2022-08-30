0aed4100a5198581fec79a259b577ce0acf3ead9
at4 <at4@jc4.so-net.ne.jp>
2022-08-30 09:54:57 +0200

    Add eslint exclusion rules locally

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./scss/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
