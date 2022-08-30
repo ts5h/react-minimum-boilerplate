855f9e2ba5b5b224e6ef218763b19266ce05584c
at4 <at4@jc4.so-net.ne.jp>
2022-08-30 10:02:19 +0200

    Update code

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
+
 root.render(
   <React.StrictMode>
     <App />
