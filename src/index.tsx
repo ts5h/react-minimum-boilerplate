87fafbd9dcbd908b6fab2816e52cb6f61a3a4d5a
at4 <at4@jc4.so-net.ne.jp>
2024-10-24 01:17:00 +0200

    Remove the web-vitals package

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./scss/index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(document.getElementById("root")!);
@@ -13,7 +12,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
